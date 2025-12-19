161a41cac06035946d0a70921cc5b8c01e0a7864
HocTrinh-cmd <tth06102004@gmail.com>
2025-12-19 19:42:45 +0700

    [FIX] route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import VerifyEmail from './pages/VerifyEmail';
-import Login from './pages/Login';
-import ResetPassword from './pages/resetPassword';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VerifyEmail from "./pages/VerifyEmail";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/resetPassword";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<div>Trang chủ Movie</div>} />
+
+        <Route path="/login" element={<Login />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+
+        <Route path="*" element={<div>404 - Không tìm thấy trang</div>} />
       </Routes>
     </BrowserRouter>
   );
